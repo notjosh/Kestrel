0f13200ea77c5b813744909b3ef7818318391a0a
Joshua May <notjosh@gmail.com>
2018-06-05 10:52:15 +0200

    Initial Commit

---
+++ Blotchy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Blotchy
+//
+//  Created by Joshua May on 5/6/18.
+//  Copyright © 2018 Joshua May. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
