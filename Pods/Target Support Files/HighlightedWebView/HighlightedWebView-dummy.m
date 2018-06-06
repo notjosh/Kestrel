ae9dc83c9897ee32d9572e1e04f03c2c6da58d41
Joshua May <notjosh@gmail.com>
2018-06-06 02:32:25 +0200

    CocoaPods gitignored

--- Pods/Target Support Files/HighlightedWebView/HighlightedWebView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_HighlightedWebView : NSObject
-@end
-@implementation PodsDummy_HighlightedWebView
-@end
--- Pods/Target Support Files/Pods-Blotchy/Pods-Blotchy-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Blotchy : NSObject
-@end
-@implementation PodsDummy_Pods_Blotchy
-@end
